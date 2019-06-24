24f05172a338dee0d48e15b9d36e73f20381a4ce
Paulo Lima <pflima92@gmail.com>
2019-06-24 16:36:06 -0300

    Update dependency and default values.

--- src/config.js
+++ src/config.js
@@ -9,8 +9,8 @@ export const getConfig = () => {
     let configAsString = localStorage.getItem('config');
     let config = JSON.parse(configAsString);
     return {
-        baseUrl: (config && config.baseUrl) || 'http://localhost:5000',
-        websocket: (config && config.websocket) || 'ws://localhost:5000',
+        baseUrl: (config && config.baseUrl) || 'http://localhost:3000',
+        websocket: (config && config.websocket) || 'ws://localhost:3000',
         version: version,
         ...config
     };
